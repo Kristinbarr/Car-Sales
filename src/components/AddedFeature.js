760cc9ec91740e2fb3d4f2564050ccd16e2c0a18
Kristin Barr <kristinbarr@gmail.com>
2019-08-14 19:14:21 -0400

    connected addedFeatures and click handler

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux'
+
+import { removeAddedFeature } from '../actions'
 
 const AddedFeature = props => {
   return (
     <li>
-      {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button onClick={()=> props.removeAddedFeature(props.feature)} className="button">X</button>
       {props.feature.name}
     </li>
   );
 };
 
-export default AddedFeature;
+const mapStateToProps = (state) => {
+  return {
+    car: state.car
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  { removeAddedFeature }
+)(AddedFeature)
--- src/components/AddedFeatures.js
+++ src/components/AddedFeatures.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React from 'react'
+import { connect } from 'react-redux'
 
-import AddedFeature from './AddedFeature';
+import AddedFeature from './AddedFeature'
 
-const AddedFeatures = props => {
+const AddedFeatures = (props) => {
   return (
-    <div className="content">
+    <div className='content'>
       <h6>Added features:</h6>
       {props.car.features.length ? (
-        <ol type="1">
-          {props.car.features.map(item => (
+        <ol type='1'>
+          {props.car.features.map((item) => (
             <AddedFeature key={item.id} feature={item} />
           ))}
         </ol>
@@ -16,7 +17,16 @@ const AddedFeatures = props => {
         <p>You can purchase items from the store.</p>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default AddedFeatures;
+const mapStateToProps = (state) => {
+  return {
+    car: state.car
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  {}
+)(AddedFeatures)
