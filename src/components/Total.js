d1a68ba75df63566214043b2948a2ab0871ec81b
Kristin Barr <kristinbarr@gmail.com>
2020-05-05 01:21:51 -0400

    REconfigured Total with useSelector hooks

--- src/components/Total.js
+++ src/components/Total.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import { connect } from 'react-redux'
+import React from 'react'
+import { connect, useSelector } from 'react-redux'
 
-const Total = props => {
+const Total = () => {
+  // useSelector allows us to "select" data from state
+  const car = useSelector(state => {
+    return state.car
+  })
+
+  // useSelector takes the state and returns what we want from state
+  const additionalPrice = useSelector(state => {
+    return state.additionalPrice
+  })
+
   return (
-    <div className='columns'>
-      <h4 className='subtitle column is-offset-4'>Total Amount: ${props.car.price + props.additionalPrice}</h4>
+    <div className="columns">
+      <h4 className="subtitle column is-offset-4">
+        Total Amount: ${car.price + additionalPrice}
+      </h4>
     </div>
-  );
-};
-
-const mapStateToProps = (state) => {
-  return {
-    additionalPrice: state.additionalPrice,
-    car: state.car,
-  }
+  )
 }
 
-export default connect(mapStateToProps, {})(Total);
+export default Total
