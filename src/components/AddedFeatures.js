a239934baa068dacdc8403ef666a80ba31118db9
Kristin Barr <kristinbarr@gmail.com>
2020-05-05 02:21:57 -0400

    added useSelector

--- src/components/AddedFeatures.js
+++ src/components/AddedFeatures.js
@@ -1,32 +1,28 @@
 import React from 'react'
-import { connect } from 'react-redux'
+import { useSelector } from 'react-redux'
 
 import AddedFeature from './AddedFeature'
 
-const AddedFeatures = (props) => {
+const AddedFeatures = () => {
+  
+  const features = useSelector(state => {
+    return state.car.features
+  })
+
   return (
-    <div className='section message is-primary'>
-      <h6 className='message-header'>Added features:</h6>
-      {props.car.features.length ? (
-        <ol type='1'>
-          {props.car.features.map((item) => (
+    <div className="section message is-primary">
+      <h6 className="message-header">Added features:</h6>
+      {features.length ? (
+        <ol type="1">
+          {features.map(item => (
             <AddedFeature key={item.id} feature={item} />
           ))}
         </ol>
       ) : (
-        <p className='message-body'>You can purchase items from the store.</p>
+        <p className="message-body">You can purchase items from the store.</p>
       )}
     </div>
   )
 }
 
-const mapStateToProps = (state) => {
-  return {
-    car: state.car
-  }
-}
-
-export default connect(
-  mapStateToProps,
-  {}
-)(AddedFeatures)
+export default AddedFeatures
