35c012ccac067efe8b85912f437a87a513e56ab8
Kristin Barr <kristinbarr@gmail.com>
2020-05-05 02:20:06 -0400

    formatting updates

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,14 +5,14 @@ const initialState = {
     name: '2019 Ford Mustang',
     image:
       'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-    features: []
+    features: [],
   },
   store: [
     { id: 1, name: 'V-6 engine', price: 1500 },
     { id: 2, name: 'Racing detail package', price: 1500 },
     { id: 3, name: 'Premium sound system', price: 500 },
-    { id: 4, name: 'Rear spoiler', price: 250 }
-  ]
+    { id: 4, name: 'Rear spoiler', price: 250 },
+  ],
 }
 
 export const reducer = (state = initialState, action) => {
@@ -23,9 +23,9 @@ export const reducer = (state = initialState, action) => {
         additionalPrice: (state.additionalPrice += action.payload.price),
         car: {
           ...state.car,
-          features: [...state.car.features, action.payload]
+          features: [...state.car.features, action.payload],
         },
-        store: state.store.filter((feature) => feature.id !== action.payload.id)
+        store: state.store.filter(feature => feature.id !== action.payload.id),
       }
     case 'REMOVE_ADDED_FEATURE':
       return {
@@ -34,10 +34,10 @@ export const reducer = (state = initialState, action) => {
         car: {
           ...state.car,
           features: state.car.features.filter(
-            (feature) => feature.id !== action.payload.id
-          )
+            feature => feature.id !== action.payload.id
+          ),
         },
-        store: [...state.store, action.payload]
+        store: [...state.store, action.payload],
       }
     default:
       return state
